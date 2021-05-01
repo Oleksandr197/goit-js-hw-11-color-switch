f3a9af424f23d4b8f943fd320810d5cc8a9406d9
Oleksandr197 <sealanch73@gmai.com>
2021-05-01 16:14:09 +0300

    CS

---
+++ index.js
@@ -0,0 +1,42 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+const bodyRef = document.querySelector("body");
+const startBtnRef = document.querySelector('button[data-action="start"]');
+const stopBtnRef = document.querySelector('button[data-action="stop"]');
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const colorSwitch = {
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    
+    this.intervalId = setInterval(() => {
+      
+      bodyRef.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length-1)];
+      
+    }, 1000);
+  },
+  stop() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+    
+  }
+};
+
+startBtnRef.addEventListener("click", colorSwitch.start.bind(colorSwitch));
+stopBtnRef.addEventListener("click", colorSwitch.stop.bind(colorSwitch));
